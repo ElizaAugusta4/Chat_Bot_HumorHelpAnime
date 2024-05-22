b5c9f2673b8fc4575bf891eac6e36a6917f6f6d4
Eliza_Carvalho <elizaaugusta71@gmail.com>
2024-05-22 19:48:50 -0300

    Alterações

--- anime_recommender/recommendations/urls.py
+++ anime_recommender/recommendations/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_anime_by_genre, name='recommend'),
+    path('', views.recommend_view, name='recomendar_animes'),
 ]
--- anime_recommender/recommendations/views.py
+++ anime_recommender/recommendations/views.py
@@ -3,83 +3,65 @@ from .models import UserPreference
 from .services import get_anime_by_genre
 import requests
 
+def recommend_view(request):
+    stage = request.session.get('stage', 'greeting')
+    message = ''
+    anime_list = []
+    
+    if stage == 'greeting':
+        message = 'Olá, tudo bem?'
+        request.session['stage'] = 'ask_genre'
+    elif stage == 'ask_genre':
+        message = 'Qual gênero de animes você gosta?'
+        if request.method == 'POST':
+            genre = request.POST.get('genre')
 
-def recommend(request):
-    if request.method == 'POST':
-        genre = request.POST['genre']
-        animes = get_anime_by_genre(genre)
-        return render(request, 'recommend.html', {'animes': animes})
-    else:
-        return render(request, 'recommend.html')
-
-
-
-
-def get_anime_by_genre(request):
-    if request.method == 'POST':
-        genre = request.POST.get('genre')
-
-        url = 'https://graphql.anilist.co'
-        query = """
-        query ($genre: String) {
-          Page (page: 1, perPage: 5) {
-            media (genre: $genre, type: ANIME) {
-              title {
-                romaji
-                english
-                native
-              }
-              genres
-              coverImage {
-                large
+            url = 'https://graphql.anilist.co'
+            query = """
+            query ($genre: String) {
+              Page (page: 1, perPage: 5) {
+                media (genre: $genre, type: ANIME) {
+                  title {
+                    romaji
+                    english
+                    native
+                  }
+                  genres
+                  coverImage {
+                    large
+                  }
+                }
               }
             }
-          }
-        }
-        """
-        variables = {
-            'genre': genre
-        }
+            """
+            variables = {
+                'genre': genre
+            }
 
-        response = requests.post(url, json={'query': query, 'variables': variables})
-        data = response.json()
+            response = requests.post(url, json={'query': query, 'variables': variables})
+            data = response.json()
 
-        anime_list = []
+            for anime in data['data']['Page']['media']:
+                title = anime["title"]["romaji"]
+                genres = anime["genres"]
+                image_url = anime["coverImage"]["large"]
 
-        for anime in data['data']['Page']['media']:
-            title = anime["title"]["romaji"]
-            genres = anime["genres"]
-            image_url = anime["coverImage"]["large"]
+                anime_list.append({
+                    "images_jpg": image_url,
+                    "title": title,
+                    "genres": genres,
+                })
 
-            anime_list.append({
-                "images_jpg": image_url,
-                "title": title,
-                "genres": genres,
-            })
+                # Create and save a UserPreference instance
+                user_preference = UserPreference.objects.filter(
+                    genre=genre,
+                    title=title
+                )
 
-            # Create and save a UserPreference instance
-            user_preference = UserPreference.objects.filter(
-                genre=genre,
-                title=title
-            )
+                # If it doesn't exist, create it
+                if not user_preference.exists():
+                    UserPreference.objects.create(genre=genre, title=title)
 
-            # If it doesn't exist, create it
-            if not user_preference.exists():
-                UserPreference.objects.create(genre=genre, title=title)
+            request.session['stage'] = 'greeting'
 
-
-        return render(request, 'recommend.html', {'animes': anime_list})
-    else:
-        return render(request, 'recommend.html')
-class RecommendView():
-    def get(self, request):
-        stage = request.session.get('stage', 'greeting')
-        if stage == 'greeting':
-            message = 'Olá, tudo bem?'
-        elif stage == 'ask_genre':
-            message = 'Qual gênero de animes você gosta?'
-        else:
-            message = 'Desculpe, não entendi.'
-        return render(request, 'recommend.html', {'message': message})
-
-
+    return render(request, 'recommend.html', {'message': message, 'animes': anime_list})
